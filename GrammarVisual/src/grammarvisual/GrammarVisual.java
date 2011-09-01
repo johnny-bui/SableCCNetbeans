f91f8489e66ebaef350e252cb61c938fa8b257a4
verylazyboy <phucluoi@gmail.com>
2011-09-01 18:50:31 +0200

    Some of Layouts use Random. => Graph Layout is not consistence

--- GrammarVisual/src/grammarvisual/GrammarVisual.java
+++ GrammarVisual/src/grammarvisual/GrammarVisual.java
@@ -1,7 +1,19 @@
 package grammarvisual;
 
+import edu.uci.ics.jung.algorithms.layout.AggregateLayout;
+import edu.uci.ics.jung.algorithms.layout.BalloonLayout;
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.DAGLayout;
+import edu.uci.ics.jung.algorithms.layout.FRLayout;
+import edu.uci.ics.jung.algorithms.layout.FRLayout2;
+import edu.uci.ics.jung.algorithms.layout.ISOMLayout;
+import edu.uci.ics.jung.algorithms.layout.KKLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.algorithms.layout.PolarPoint;
+import edu.uci.ics.jung.algorithms.layout.RadialTreeLayout;
+import edu.uci.ics.jung.algorithms.layout.SpringLayout;
+import edu.uci.ics.jung.algorithms.layout.SpringLayout2;
+import edu.uci.ics.jung.algorithms.layout.StaticLayout;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
@@ -11,6 +23,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Paint;
 import java.awt.Stroke;
+import java.awt.geom.Point2D;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -32,8 +45,9 @@ public class GrammarVisual {
 	/**
 	 * @param args the command line arguments
 	 */
-	public static void main(String[] args) throws FileNotFoundException, ParserException, LexerException, IOException {
-
+	public static void main(String[] args) 
+			throws FileNotFoundException, ParserException, LexerException, IOException 
+	{
 		System.out.println(System.getProperty("user.dir"));
 		String smallPascal = "./src/PostFix_1.scc";
 
@@ -49,18 +63,37 @@ public class GrammarVisual {
 
 		System.out.println(dep);
 
+		Transformer<GraphNode, Point2D> locationTransformer = 
+				new Transformer<GraphNode, Point2D>() 
+		{
+            @Override
+            public Point2D transform(GraphNode vertex) {
+                int value =  20;
+                return new Point2D.Double((double) value, (double) value);
+            }
+        };
+
+		
+		CircleLayout<GraphNode,GraphLink> staticLayout = 
+				new CircleLayout<GraphNode, GraphLink>(dep/*,locationTransformer*/);
+		staticLayout.setSize(new Dimension(450, 450));
+
 		// The Layout<V, E> is parameterized by the vertex and edge types
-		Layout<GraphNode, GraphLink> layout = new CircleLayout(dep);
-		layout.setSize(new Dimension(300, 300)); // sets the initial size of the space
+		Layout<GraphNode, GraphLink> layout = 
+				new FRLayout<GraphNode, GraphLink> (dep);
+		layout.setInitializer(staticLayout);
+		
+		//layout.setSize(new Dimension(450, 450)); // sets the initial size of the space
 
 		// The BasicVisualizationServer<V,E> is parameterized by the edge types
 		BasicVisualizationServer<GraphNode, GraphLink> vv =
 				new BasicVisualizationServer<GraphNode, GraphLink>(layout);
-		vv.setPreferredSize(new Dimension(350, 350)); //Sets the viewing area size
+		vv.setPreferredSize(new Dimension(500, 500)); //Sets the viewing area size
 
 
 		Transformer<GraphNode, Paint> vertexPaint = new Transformer<GraphNode, Paint>() 
 		{
+			@Override
 			public Paint transform(GraphNode i) 
 			{
 				if (i.isToken)
@@ -77,12 +110,13 @@ public class GrammarVisual {
 		
 		Transformer<GraphLink, Stroke> edgeStrokeTransformer =
 				new Transformer<GraphLink, Stroke>() 
-				{
-					public Stroke transform(GraphLink s)
-					{// TODO: specify edges here
-						return edgeStroke;
-					}
-				};
+		{
+			@Override
+			public Stroke transform(GraphLink s)
+			{// TODO: specify edges here
+				return edgeStroke;
+			}
+		};
 		vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
 		vv.getRenderContext().setEdgeStrokeTransformer(edgeStrokeTransformer);
 		vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
