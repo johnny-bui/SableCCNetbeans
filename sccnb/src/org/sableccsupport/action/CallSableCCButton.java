d617b34137e8f7ba678d32ab15b60139ca5fffac
verylazyboy <phucluoi@gmail.com>
2011-09-15 23:52:16 +0200

    change template

---
+++ sccnb/src/org/sableccsupport/action/CallSableCCButton.java
@@ -0,0 +1,37 @@
+package org.sableccsupport.action;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import org.openide.awt.ActionRegistration;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionID;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.NbBundle.Messages;
+import org.sableccsupport.SCCDataObject;
+
+@ActionID(category = "Build",
+id = "org.sableccsupport.action.CallSableCCButton")
+@ActionRegistration(iconBase = "org/sableccsupport/sableccBuild.png",
+displayName = "#CTL_CallSableCCButton")
+@ActionReferences({
+	@ActionReference(path = "Menu/BuildProject", position = 247)
+})
+@Messages("CTL_CallSableCCButton=Call SableCC")
+public final class CallSableCCButton implements ActionListener {
+
+	private final SCCDataObject context;
+
+	public CallSableCCButton(SCCDataObject context) {
+		this.context = context;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ev) {
+		FileObject f = context.getPrimaryFile();
+		String displayName = FileUtil.getFileDisplayName(f);
+		SableCCCaller.callSableCC(displayName);
+	}
+}
