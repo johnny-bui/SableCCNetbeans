7e44d2867efbfab01681389a430c68abeeb3cbe9
Hong Phuc Bui <bui_hong_phuc@yahoo.com>
2012-04-29 22:19:23 +0200

    add new Netbeans project into rep

---
+++ AstVisual/src/de/htwsaarland/mathcoach/astVisual/DFMarkerListener.java
@@ -0,0 +1,44 @@
+package de.htwsaarland.mathcoach.astVisual;
+
+
+import java.util.Stack;
+import org.jgrapht.event.TraversalListenerAdapter;
+import org.jgrapht.event.VertexTraversalEvent;
+
+public class DFMarkerListener<V extends AstVertex,E extends AstEdge> 
+	extends TraversalListenerAdapter<AstVertex, AstEdge>
+{
+	int counter ;
+	Stack<AstVertex> visit;
+	public DFMarkerListener()
+	{
+		counter = 1;
+		visit = new Stack<AstVertex>();
+	}
+	
+	@Override
+	public void  vertexTraversed(VertexTraversalEvent<AstVertex> e) 
+	{
+		AstVertex v = e.getVertex();
+		v.setDetected(counter);
+		counter ++;
+		if (counter > 2)
+		{
+			v.setPred(visit.peek());
+		}
+		visit.push(v);
+		System.out.println("visit: " + v);
+		System.out.println("stack: " + visit);
+	}
+
+	@Override
+	public void vertexFinished(VertexTraversalEvent<AstVertex> e)
+	{
+		AstVertex v = e.getVertex();
+		v.setFinished(counter);
+		counter ++;
+		visit.pop();
+		System.out.println("exit:  " + v);
+		System.out.println("stack: " + visit);
+	}
+}
