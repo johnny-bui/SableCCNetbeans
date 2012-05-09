e93d2b59e227f1fdaf63d33b577a8346f79ac37a
verylazyboy <>
2012-05-09 23:08:13 +0200

    call SableCC in separated thread to accelerate compile phase (significant :D)

--- sccnb/src/org/sableccsupport/action/SableCCCaller.java
+++ sccnb/src/org/sableccsupport/action/SableCCCaller.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import org.openide.util.Exceptions;
 import org.sablecc.sablecc.EmbeddedSableCC;
 import org.sablecc.sablecc.SableCC;
 
@@ -19,19 +20,112 @@ public class SableCCCaller
 	
 	public static void callSableCC(String filename)
 	{
+		/*
 		redirectSystemStreams();
 		try {
 			String msg = "+++++++++++++++++" + filename + "+++++++++++++";
 			System.out.println (msg);
-			SableCC.processGrammar(filename,null);
+			Helper h = new Helper();
+			h.setup(filename);
+			h.start();
+			//SableCC.processGrammar(filename,null);
 		} catch (Exception ex) {
 			System.err.println(ex.getMessage());
 		}finally
 		{
 			setBackOutput();
 		}
-		
+		*/
+		Helper h = new Helper();
+		h.setup(filename);
+		h.start();
 	}
+
+	private static void redirectSystemStreams() 
+	{
+        OutputStream out = new OutputStream() {
+
+            @Override
+            public void write(int i) throws IOException {
+                OutputHandler.output(EmbeddedSableCC.SABLE_CC_OUTPUT_TITLE ,
+						String.valueOf((char) i));
+            }
+
+            @Override
+            public void write(byte[] bytes) throws IOException {
+                OutputHandler.output(EmbeddedSableCC.SABLE_CC_OUTPUT_TITLE, 
+						new String(bytes));
+            }
+
+            @Override
+            public void write(byte[] bytes, int off, int len) throws IOException {
+                OutputHandler.output(EmbeddedSableCC.SABLE_CC_OUTPUT_TITLE, 
+						new String(bytes, off, len));
+            }
+        };
+
+		OutputStream err = new OutputStream() {
+
+            @Override
+            public void write(int i) throws IOException {
+                OutputHandler.output(EmbeddedSableCC.SABLE_CC_OUTPUT_TITLE ,
+						String.valueOf((char) i), Color.RED);
+            }
+
+            @Override
+            public void write(byte[] bytes) throws IOException {
+                OutputHandler.output(EmbeddedSableCC.SABLE_CC_OUTPUT_TITLE, 
+						new String(bytes), Color.RED);
+            }
+
+            @Override
+            public void write(byte[] bytes, int off, int len) throws IOException {
+                OutputHandler.output(EmbeddedSableCC.SABLE_CC_OUTPUT_TITLE, 
+						new String(bytes, off, len),Color.RED);
+            }
+        };
+		orgOutStream = System.out;
+		orgErrStream = System.err;
+        System.setOut(new PrintStream(out, true));
+        System.setErr(new PrintStream(err, true));
+    }
+
+	private static void setBackOutput()
+	{
+		System.setErr(orgErrStream);
+		System.setOut(orgOutStream);
+	}
+}
+
+
+
+class Helper extends Thread
+{
+	private String filename;
+	static PrintStream orgOutStream 	= null;
+	static PrintStream orgErrStream 	= null;
+	public void setup(String filename)
+	{
+		this.filename = filename;
+	}
+
+	@Override
+	public void run() {
+		try {
+			redirectSystemStreams();
+			String msg = "+++++++++++++++++" + filename + "+++++++++++++";
+			System.out.println (msg);
+			SableCC.processGrammar(filename,null);
+		} catch (Exception ex) {
+			//Exceptions.printStackTrace(ex);
+			System.err.println(ex.getMessage());
+		}finally
+		{
+			setBackOutput();
+		}
+    }
+
+
 
 	private static void redirectSystemStreams() 
 	{
